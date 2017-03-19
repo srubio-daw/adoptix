f6fd655a2587c2ca226488671df65cdc3061cb81
sylvia <sylvia@192.168.0.10>
2017-03-19 19:30:28 +0100

    Crear tabla de usuarios #4

---
+++ src/main/resources/database/province.sql
@@ -0,0 +1,4 @@
+CREATE TABLE province (
+	id serial NOT NULL PRIMARY KEY,
+	name character varying(50) NOT NULL UNIQUE
+);
---
+++ src/main/resources/database/schema.sql
@@ -0,0 +1,19 @@
+DROP TABLE IF EXISTS "user";
+DROP TABLE IF EXISTS province;
+
+CREATE TABLE province (
+	id serial NOT NULL PRIMARY KEY,
+	name character varying(50) NOT NULL UNIQUE
+);
+
+CREATE TABLE "user" (
+	id serial NOT NULL PRIMARY KEY,
+	name character varying(25) NOT NULL,
+	surname character varying(80),
+	nif character varying(9) UNIQUE,
+	address character varying(200),
+	province_id bigint NOT NULL REFERENCES province (id),
+	mail character varying(150) NOT NULL UNIQUE,
+	username character varying(20) NOT NULL UNIQUE,
+	password character varying(32) NOT NULL
+);
